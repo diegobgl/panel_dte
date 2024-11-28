f273f3e036dee4c3e71f2194fe8467d304378d71
Diego Gajardo <deskodoo@a2group.cl>
2024-11-28 13:08:03 -0300

    ss

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,6 @@
     'depends': ['mail', 'account', 'l10n_cl_edi'],
     'data': [
         'views/invoice_mail_view.xml',
-        'views/invoice_mail_extended_view.xml',
     ],
     'installable': True,
     'auto_install': False,
