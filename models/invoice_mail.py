c18d888f347ef1b87bc07298780ee296a4687e93
Diego Gajardo <diegobgajardo@gmail.com>
2024-12-19 10:57:17 -0300

    sss

--- models/invoice_mail.py
+++ models/invoice_mail.py
@@ -5,7 +5,7 @@ from odoo.tools import email_split
 from odoo.exceptions import UserError
 import base64
 import xml.etree.ElementTree as ET
-from odoo import logging
+import logging
 
 
 _logger = logging.getLogger(__name__)
