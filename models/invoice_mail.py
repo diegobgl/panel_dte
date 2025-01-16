9ac5ba08f7a52a4de04399283476bc52b51d62af
Diego Gajardo <diegobgajardo@gmail.com>
2025-01-16 14:37:48 -0300

    ssssss

--- models/invoice_mail.py
+++ models/invoice_mail.py
@@ -10,6 +10,9 @@ import cryptography
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import pkcs12
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
 import logging
 import hashlib
 import html
