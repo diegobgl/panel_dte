44e4cbd7ce15584b66add436279d9b70a8f71c2b
Diego Gajardo <diegobgajardo@gmail.com>
2025-01-22 15:23:40 -0300

    ss

--- models/invoice_mail.py
+++ models/invoice_mail.py
@@ -13,6 +13,8 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+from odoo.tools import email_split
+
 
 _logger = logging.getLogger(__name__)
 
