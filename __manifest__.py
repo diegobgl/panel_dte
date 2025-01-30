2cf5717f9f0111a51109c6956f0a02cd8f9f6206
Diego Gajardo <diegobgajardo@gmail.com>
2025-01-30 19:24:15 -0300

    sss

--- __manifest__.py
+++ __manifest__.py
@@ -7,12 +7,12 @@
     'author': 'Diego Gajardo',
     'depends': ['mail', 'account', 'l10n_cl_edi'],
     'data': [
+        'wizards/dte_reclamo_wizard.xml',
         'views/invoice_mail_view.xml',
         'views/invoice_mail_report.xml',
         'views/config_setting.xml',
         'report/invoice_mail_report_templates.xml',
         'security/ir.model.access.csv',
-        'wizards/dte_reclamo_wizard.xml',
 
     ],
     'installable': True,
